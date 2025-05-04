c93d9dfae2288300d8f09790c25851f966cdc489
jinuklee <150532431+jinuk0211@users.noreply.github.com>
2025-05-05 00:45:51 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from generator import retriever
   
 if __name__ == "__main__":
-  doc = retriever.search_document_demo("What is the relationship between the lifetime of a quantum state and its energy uncertainty?",1)
-  print(doc)
+  doc = retriever.search_document_demo("What is the relationship between the lifetime of a quantum state and its energy uncertainty?",2)
+  for retrieved_document in doc:
+      print(retrieved_document['text'])
