d1a4037f4da74b78e39e33a2e91c0eb1687b3558
jinuklee <150532431+jinuk0211@users.noreply.github.com>
2025-05-07 02:34:41 +0900

    Update naive_rag.py

--- naive_rag.py
+++ naive_rag.py
@@ -11,49 +11,25 @@ import time
 
 tokenizer, model = load_vLLM_model(cfg.model_ckpt, cfg.seed, cfg.tensor_parallel_size, cfg.half_precision)
 ds = load_dataset("Idavidrein/gpqa", "gpqa_diamond")
-
+split = 2
+df = ds['train'].select(range(split)).to_pandas()
+output_list = []
+input_list = []
 if __name__ == "__main__":
   # retriever.search_document_demo("What is the relationship between the lifetime of a quantum state and its energy uncertainty?",1)
 # for i in range(len(ds['train']['Question'])):
   with torch.no_grad():
     for i in range(2):
         question = ds['train']['Question'][i]
+        input_list.append(question)
         retrieved_document = retriever.search_document_demo(question, 1)
         response = generate_with_vLLM_model(model,rag_prompt.format(context=retrieved_document['text'],question=question))
         final_input = 'output:' + response _ "Therefore, the answer is "
         only_answer = generate_with_vLLM_model(model,final_input,max_tokens=20)
-
+        output_list.append(only_answer)
     torch.cuda.empty_cache()
-    evaluator.evaluate(only_answer, ds['train']['Correct Answer'][i])
+    # evaluator.evaluate(only_answer, ds['train']['Correct Answer'][i])
+    run_evaluate(df,input_list,output_list, output_dir='/workspace/output')
 
 
 
-def generate_with_vLLM_model(
-    model,
-    input,
-    temperature=0.8,
-    top_p=0.95,
-    top_k=40,
-    repetition_penalty=1.1,
-    n=1,
-    max_tokens=256,
-    logprobs=1,
-    stop=[],
-):
-    sampling_params = SamplingParams(
-        temperature=temperature,
-        top_p=top_p,
-        top_k=top_k,
-        repetition_penalty=repetition_penalty,
-        n=n,
-        logprobs=logprobs,
-        max_tokens=max_tokens,
-        stop=stop,)
-
-    output = model.generate(input, sampling_params, use_tqdm=False)
-    return output
-
-
-
-
-  
